2618e2cfb9152eae2325acd7dea7673d933006fe
chndr1 <bhargav.annem@gmail.com>
2023-05-16 10:13:24 -0500

    attempt to fix opengl issue

--- assets/fragment_core.glsl
+++ assets/fragment_core.glsl
@@ -1,8 +1,13 @@
 #version 330 core
 in vec3 color;
+in vec2 TexCoord;
 out vec4 FragColor;
+
+uniform sampler2D texture1;
+uniform sampler2D texture2;
 
 void main() {
     // FragColor = vec4(1.0f, 0.2, 0.6f, 1.0f);
-    FragColor = vec4(color, 1.0);
+    // FragColor = vec4(color, 1.0);
+    FragColor = texture(texture1, TexCoord);
 }
--- main.cpp
+++ main.cpp
@@ -58,18 +58,18 @@ int main()
 
     // Shaders
     Shader shader("./assets/vertex_core.glsl", "./assets/fragment_core.glsl");
-    Shader shader2("./assets/vertex_core.glsl", "./assets/fragment_core2.glsl");
 
     float vertices[] = {
-        // first triangle
-        -0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 0.5f,
-        0.5f, 0.5f, 0.0f, 0.5f, 1.0f, 0.75f,
-        0.5f, -0.5f, 0.0f, 0.6f, 1.0f, 0.2f,
-        -0.5f, 0.5f, 0.0f, 0.3f, 0.1f, 0.8f};
-
+        // positions		// colors			// texture coordinates
+        -0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 0.5f, 0.0f, 0.0f, // bottom left
+        -0.5f, 0.5f, 0.0f, 0.5f, 1.0f, 0.75f, 0.0f, 1.0f, // top left
+        0.5f, -0.5f, 0.0f, 0.6f, 1.0f, 0.2f, 1.0f, 0.0f,  // bottom right
+        0.5f, 0.5f, 0.0f, 1.0f, 0.2f, 1.0f, 1.0f, 1.0f    // top right
+    };
     unsigned int indices[] = {
-        0, 1, 2,
-        0, 3, 1};
+        0, 1, 2, // first triangle
+        3, 1, 2  // second triangle
+    };
 
     // VAO/VBO
     unsigned int VAO,
@@ -89,28 +89,68 @@ int main()
     // set attributes
 
     // positions
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
 
     // color
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
     // set up EBO
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    // rotation matrix
-    glm::mat4 trans = glm::mat4(1.0f);
-    trans = glm::rotate(trans, glm::radians(45.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-    shader.activate();
-    shader.setMat4("transform", trans);
+    // texture coords
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
 
-    glm::mat4 trans2 = glm::mat4(1.0f);
-    trans2 = glm::scale(trans2, glm::vec3(1.5f));
-    trans2 = glm::rotate(trans2, glm::radians(15.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-    shader2.activate();
-    shader2.setMat4("transform", trans);
+    // textures
+    unsigned int texture1, texture2;
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+    // load image
+    int width, height, nChannels;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char *data = stbi_load("./assets/obama.jpg", &width, &height, &nChannels, 0);
+
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture\n";
+    }
+
+    stbi_image_free(data);
+
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+
+    data = stbi_load("./assets/trump.jpg", &width, &height, &nChannels, 0);
+
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture\n";
+    }
+
+    stbi_image_free(data);
+
+    shader.activate();
+    shader.setInt("texture1", 0);
+    shader.setInt("texture2", 1);
 
     while (!glfwWindowShouldClose(window))
     {
@@ -120,21 +160,16 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        trans = glm::rotate(trans, glm::radians((float)glfwGetTime() / 100.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-        shader.activate();
-        shader.setMat4("transform", trans);
-
-        trans2 = glm::rotate(trans2, glm::radians((float)glfwGetTime() / -100.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-        shader2.activate();
-        shader2.setMat4("transform", trans2);
-
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
         // draw shapes
         glBindVertexArray(VAO);
         shader.activate();
-        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
-        shader2.activate();
-        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, (void *)(3 * sizeof(GLuint)));
+        glBindVertexArray(0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
